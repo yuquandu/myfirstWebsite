6b313dd5670f78d254d61ddd5bc1425b4c97aa57
EC2 Default User <ec2-user@ip-172-31-38-61.ap-southeast-2.compute.internal>
2023-06-18 05:31:39 +0000

    initial commit

---
+++ Python/helloPythoninAW.py
@@ -0,0 +1,4 @@
+print("Hello, Python in AWS Cloud9 IDE")
+utas = "I used to be a lecturer in University of Tasmania."
+ltu = " But now I am working at Latrboe Univesity."
+print(utas+ltu)
---
+++ Webpages/createReactApp.js
@@ -0,0 +1,3 @@
+npx create-react-app amplifyapp
+cd amplifyapp
+npm start
